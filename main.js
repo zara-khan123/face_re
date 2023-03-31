69ab6003e5a03140c0d658c4ac66ed8ea8f99885
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-03-31 21:39:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
+webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_url)
+    {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">'
+    });
+}
+
+console.log('ml5 version',  ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1j1z7VDFN/model.json');
